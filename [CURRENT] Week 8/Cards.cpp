3f9e0c20cceb8e8af4bdee75f521251c4bed8d2d
William Lin <wlin26@yahoo.com>
2015-10-16 14:32:20 -0400

    Card program done

--- [CURRENT] Week 8/Cards.cpp
+++ [CURRENT] Week 8/Cards.cpp
@@ -1,6 +1,9 @@
 #include "tools.hpp"
 #include "tools.cpp"
 
+string suits[13] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+string ranks[4] = {"Diamonds", "Hearts", "Clubs", "Spades"}; 
+
 class Cards{
 	private:
 		int suit, rank;
@@ -12,18 +15,32 @@ class Cards{
 			suit = x;
 			rank = y;
 		}
-		void buildDeck(){
-			Cards deck[52];
+
+		void printCard(){
+			std::cout << suits[suit] << " of ";
+			std::cout << ranks[rank] << endl;
+		}
+
+		std::vector<Cards> buildDeck(){
+			std::vector<Cards> deck(52);
 			int index = 0;
-			for(int suit = 0; suit < 4; suit++){
-				for(int rank = 1; rank < 14; rank++){
+			for(int suit = 0; suit < 13; suit++){
+				for(int rank = 0; rank < 4; rank++){
 					deck[index] = Cards();
 					deck[index].setCard(suit, rank);
+					deck[index].printCard();
 					index++;
 				}
 			}
+			return deck;
+		}
+
+		void printDeck(std::vector<Cards> deck){
+		
 		}
 };
 
 int main(){
+	Cards card;
+	std::vector<Cards> deck = card.buildDeck();
 }
