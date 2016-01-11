4348dddb80a48501f6cb2df1e868b3272c6c45f4
William Lin <wlin26@yahoo.com>
2016-01-11 11:40:08 -0500

    Added folder for steganography program

---
+++ [WORK] Week 12/Steganography/Steganography/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Steganography
+//
+//  Created by William Lin on 11/17/15.
+//  Copyright © 2015 William Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ [WORK] Week 12/pgm.hpp
@@ -0,0 +1,29 @@
+/* -----------------------------------------------------------------------------
+ * pgm.h: Read and Write a .pgm image file.
+ * Created on: Oct. 23, 2015
+ * Author: Michael Fischer and Alice E. Fischer												 
+ */
+#pragma once
+#include "tools.hpp"
+
+// pgm structure ---------------------------------------------------------------
+typedef char pixel;
+
+class Pgm {
+private:
+	char fileType[3] = { '\0' }; // two magic bytes as a null-terminated string
+	int width = 0;
+	int height = 0;
+	int maxGrey = 0;
+	pixel* image = nullptr;
+public:
+    Pgm( const char* fileName );
+    ~Pgm() { delete image; }
+
+    void printHead( ostream& out ) const {
+        out << fileType << "\n" << width << " " << height << "\n"
+             << maxGrey << endl;
+    }
+    void write( const char* fileName );
+    void reverse();
+};
